c7f0ad3e8314e644824bafe5009adffcf3a192ae
rayhanmp <90276351+rayhanmp@users.noreply.github.com>
2022-04-13 21:44:20 +0700

    More update to the register module
    Functions are now more modular

--- register.py
+++ register.py
@@ -1,14 +1,15 @@
+from load import countRow
+
 def counter(item):
     count = 0
-    for i in item:
+    for element in item:
         count += 1
     return count
 
-def registerUser():
-    uname = input("Masukan username: ")
-
-    count = 0
+def cekChar(uname):
+    charValid = False
     i = 0
+    count = 0
     while i < counter(uname):
         if (ord(uname[i])>=48 and ord(uname[i])<=57) or (ord(uname[i])>=65 and ord(uname[i])<=90) or (ord(uname[i])>=97 and ord(uname[i])<=122) or (ord(uname[i])==95) or (ord(uname[i])==45):
             count += 1
@@ -17,8 +18,44 @@ def registerUser():
             i += 1
 
     if count == counter(uname):
-        print("Valid dong kk")
-    else:
-        print("Mntapphhh salah looooo")
+        return True
 
-registerUser()
+def cekUnik(uname, user):
+    n_row = countRow(user)
+    while i > n_row:
+        if user[i][1] != uname:
+            i += 1    
+        else:
+            break
+    if i != n_row:
+        return True
+    else:
+        return False
+    
+    
+
+def registerUser(user):
+
+    name = input("Masukan nama: ")
+    uname = input("Masukan username: ")
+    password = input("Masukan password: ")
+
+    n_row = countRow(user)
+    used = False
+    if cekUnik:
+        if cekChar:
+            data = []
+            data = data + [(n_row+1)]
+            data = data + [uname]
+            data = data + [name]
+            data = data + [password]
+            data = data + ["user"]
+            data = data + [0]
+            user = user + [data]
+            print(user)
+            print("Username", uname, "telah berhasil register ke dalam “Binomo”.")
+        else:
+            print("Username mengandung karakter illegal.")
+    else:
+        print("Username", uname, "sudah terpakai, silakan menggunakan username lain.")
+    return user
---
+++ test aja.py
@@ -0,0 +1,15 @@
+data = []
+print(data)
+n_row = 5
+uname = "erensss"
+name = "Budiman Suja"
+password = "mikasah"
+
+data = data + [(n_row+1)]
+data = data + [uname]
+data = data + [name]
+data = data + [password]
+data = data + ["user"]
+data = data + [0]
+
+print(data)
